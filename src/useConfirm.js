a10c386b636fe8d458bc8f2a3b1e64c918e3f0f1
SaenaKIM <tosk3927@gmail.com>
2022-07-08 18:33:43 +0900

    [TIL] useConfirm - improve parameter validation check

--- src/useConfirm.js
+++ src/useConfirm.js
@@ -1,13 +1,16 @@
-export const useConfirm = (message, callback, rejection) => {
-  if (typeof callback != "function" ) {
+export const useConfirm = (message, onConfirm, onCancel) => {
+  if (!onConfirm || typeof onConfirm !== "function" ) {
+    return;
+  }
+  if (!onCancel || typeof onCancel !== "function" ) {
     return;
   }
   const confirmAction = () => {
     if (confirm(message)) {
-      callback();
+      onConfirm();
     } 
     else {
-      rejection();
+      onCancel();
     }
   }
   return confirmAction;
