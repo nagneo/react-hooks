196b3ffdb40d3b9adc7e353c2af044f56330703c
SaenaKIM <tosk3927@gmail.com>
2022-07-23 21:19:45 +0900

    [TIL] useNotification

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import { useFullScreen } from "./useFullscreen";
-
+import { useNotification
+ } from "./useNotification";
 const App = () => {
-  const onFullS = isFull => console.log(isFull ? "We are full" : "We are small");
-  const { element, toggleFull} = useFullScreen(onFullS);
+  const triggerNotif = useNotification("Can I steal your kimchi", {body:"I love kimchi"});
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <div ref={element} >
-        <img src="https://m.media-amazon.com/images/I/71cmfJ3U0HL._AC_SL1414_.jpg" alt="grape" width="500"/>
-        <button onClick={toggleFull}>Full On/Off</button>
-      </div>
+      <button onClick={triggerNotif}>Hello</button>
     </div>
   );
 };
