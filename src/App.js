67c1cd5ffe207084b92970be61777c45b660ccd5
SaenaKIM <tosk3927@gmail.com>
2022-08-24 20:51:06 +0900

    [TIL] useAxios - last hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import { useNotification
- } from "./useNotification";
-const App = () => {
-  const triggerNotif = useNotification("Can I steal your kimchi", {body:"I love kimchi"});
-  return (
-    <div className="App" style={{ height: "1000vh" }}>
-      <button onClick={triggerNotif}>Hello</button>
-    </div>
-  );
-};
 
-export default App;
+import { ReactDOM } from "react-dom";
+import useAxios from "./useAxios/useAxios";
+
+const App = () => {
+  const { loading, data, refetch} = useAxios({
+    url: "https://yts.mx/api/v2/list_movies.json" 
+  });
+
+  console.log(`Loading: ${loading}\nData: ${data}\nError: ${error}\n`)
+  return (
+    <div className="App" style={{ height: "1000vh"}}>
+      <h1>{data && data.status}</h1>
+      <h2>{loading && "Loading"}</h2>
+      <button onClick={refetch}>Refetch</button>  
+    </div>
+  )
+  
+}
---
+++ src/useAxios/index.js
@@ -0,0 +1,2 @@
+
+export { useAxios as default } from "./useAxios";
